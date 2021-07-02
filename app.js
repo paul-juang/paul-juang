937845f424d11589ddc7138d77d63d8e07b0b15b
paul-juang <pauljuang3@gmail.com>
2021-07-02 15:06:05 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const path = require('path');
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/",function(req, res) {
+  res.render("socket");
+});
+
+io.on('connection', function(socket) {
+  console.log('Client connected: '+socket.id)
+
+  socket.on('message',function(data) {
+    socket.broadcast.emit("message",data)
+  })
+
+})
+
+server.listen(PORT, function() {
+  console.log('server running on '+ PORT) 
+})
