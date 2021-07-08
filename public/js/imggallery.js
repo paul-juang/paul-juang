678823ae951d0b277063cc0e16282f4a100d9204
paul-juang <pauljuang3@gmail.com>
2021-07-09 02:48:03 +0800

    update urlfile/imggallery.js

--- public/js/imggallery.js
+++ public/js/imggallery.js
@@ -101,15 +101,74 @@ $(function() {
      imgFav186,imgFav187,imgFav188,imgFav189,imgFav190,
      imgFav191,imgFav192,imgFav193,imgFav194,imgFav195,
      imgFav196,imgFav197,imgFav198,imgFav199,imgFav200
-  ];     
+  ];
+
+  let imgUrlArr10 = [
+     imgFav201,imgFav202,imgFav203,imgFav204,imgFav205,
+     imgFav206,imgFav207,imgFav208,imgFav209,imgFav210,
+     imgFav211,imgFav212,imgFav213,imgFav214,imgFav215,
+     imgFav216,imgFav217,imgFav218,imgFav219,imgFav220
+  ];
+
+ let imgUrlArr11 = [
+     imgFav221,imgFav222,imgFav223,imgFav224,imgFav225,
+     imgFav226,imgFav227,imgFav228,imgFav229,imgFav230,
+     imgFav231,imgFav232,imgFav233,imgFav234,imgFav235,
+     imgFav236,imgFav237,imgFav238,imgFav239,imgFav240
+  ];
+
+ let imgUrlArr12 = [
+     imgFav241,imgFav242,imgFav243,imgFav244,imgFav245,
+     imgFav246,imgFav247,imgFav248,imgFav249,imgFav250,
+     imgFav251,imgFav252,imgFav253,imgFav254,imgFav255,
+     imgFav256,imgFav257,imgFav258,imgFav259,imgFav260
+  ];
+
+  let imgUrlArr13 = [
+     imgFav261,imgFav262,imgFav263,imgFav264,imgFav265,
+     imgFav266,imgFav267,imgFav268,imgFav269,imgFav270,
+     imgFav271,imgFav272,imgFav273,imgFav274,imgFav275,
+     imgFav276,imgFav277,imgFav278,imgFav279,imgFav280
+  ];
+
+  let imgUrlArr14 = [
+     imgFav281,imgFav282,imgFav283,imgFav284,imgFav285,
+     imgFav286,imgFav287,imgFav288,imgFav289,imgFav290,
+     imgFav291,imgFav292,imgFav293,imgFav294,imgFav295,
+     imgFav296,imgFav297,imgFav298,imgFav299,imgFav300
+  ];
+
+  let imgUrlArr15 = [
+     imgFav301,imgFav302,imgFav303,imgFav304,imgFav305,
+     imgFav306,imgFav307,imgFav308,imgFav309,imgFav310,
+     imgFav311,imgFav312,imgFav313,imgFav314,imgFav315,
+     imgFav316,imgFav317,imgFav318,imgFav319,imgFav320
+  ];
+
+  let imgUrlArr16 = [
+     imgFav321,imgFav322,imgFav323,imgFav324,imgFav325,
+     imgFav326,imgFav327,imgFav328,imgFav329,imgFav330,
+     imgFav331,imgFav332,imgFav333,imgFav334,imgFav335,
+     imgFav336,imgFav337,imgFav338,imgFav339,imgFav340
+  ];
+
+  let imgUrlArr17 = [
+     imgFav341,imgFav342,imgFav343,imgFav344,imgFav345,
+     imgFav346,imgFav347,imgFav348,imgFav349,imgFav350,
+     imgFav351,imgFav352,imgFav353,imgFav354,imgFav355,
+     imgFav356,imgFav357,imgFav358,imgFav359,imgFav360
+  ];
 
 
  let imgUrlAll = [];
 
  imgUrlAll.push(imgUrlArr0,imgUrlArr1,imgUrlArr2,imgUrlArr3,imgUrlArr4,
-                imgUrlArr5,imgUrlArr6,imgUrlArr7,imgUrlArr8,imgUrlArr9);
+                imgUrlArr5,imgUrlArr6,imgUrlArr7,imgUrlArr8,imgUrlArr9,
+                imgUrlArr10,imgUrlArr11,imgUrlArr12,imgUrlArr13,imgUrlArr14,
+                imgUrlArr15,imgUrlArr16,imgUrlArr17
+                );
      
- let max0 = 10;
+ let max0 = 18;
  let m = Math.floor(Math.random()*max0);
  let imgUrlArr = imgUrlAll[m];
     
