6a044703d88d6763aea32c3b9eb0ffe1e4d932dd
paul-juang <pauljuang3@gmail.com>
2022-07-08 10:00:26 +0800

    update app/account.js

---
+++ models/account.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let accountSchema = new Schema ({
+	date: { type: String},
+    acctno: { type: String, required: true},
+    acctname: { type: String, required: true},
+    dr: { type: Number, default: 0},
+    cr: { type: Number, default: 0},
+    reference: { type: String}
+});
+
+module.exports = mongoose.model('Account',accountSchema);
